33d052ee0c72500bd36c0cc6171c2bc91efde27c
kawika valentine <kawika@kawikapedia.com>
2014-03-13 20:20:34 -0400

    assignment comments - personal

--- Expressions/Expressions_Personal/js/personal-script.js
+++ Expressions/Expressions_Personal/js/personal-script.js
@@ -1,12 +1,16 @@
 //Kawika Valentine 3/14/14 Expressions Personal
 //Personal Calculator Script
 
+//This prompt will collect how many chocolates are eaten per day
 var candyAmount = prompt("This Calculator will determine how much money you spend on Ferrero Rocher chocolates each year. \nPlease enter how many Ferrero Rocher Chocolates you eat per day:");
 
+//This prompt will collect how many days per week the chocolates are eaten
 var perWeek = prompt("Please enter how many days per week you eat Ferrero Rochers:");
 
+//This prompt will collect  how much each pack costs
 var costPerPack  = prompt("Please enter the cost per pack of Ferrero Rochers:");
 
+//This formula will calculate the annual cost
 var annualCost = candyAmount * perWeek * 52 * costPerPack;
 
 console.log(annualCost);
