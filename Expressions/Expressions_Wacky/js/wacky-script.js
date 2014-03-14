d2ea29185ca9fb4aac5b2060cbf6c37b1b66a48c
kawika valentine <kawika@kawikapedia.com>
2014-03-13 21:42:40 -0400

    scripting wacky

---
+++ Expressions/Expressions_Wacky/js/wacky-script.js
@@ -0,0 +1,13 @@
+//Kawika Valentine 3/14/14 Expressions Industry Specific//
+//Wacky Calculator//
+
+var colors = prompt ("This Wacky Calculator will determine just which Spongebob character you are most like. \nPlease choose your favorite color: \n 0: Yellow | 1: Red | 2: Green | 3: Pink") ["Yellow", "Red", "Green", "Pink"]; //0,1,2,3
+
+var food = prompt ("Select your favorite food. \n 0: Pizza | 1: Krabby Patties | 2: Peanuts | 3: Haute Cusine") ["Pizza", "Krabby Patties", "Peanuts", "Haute Cuisine"]; //0,1,2,3
+
+characterNames[0] = "Spongebob";
+characterNames[1] = "Patrick";
+characterNames[3] = "Mr. Crabs";
+characterNames[4] = "Squidward";
+
+var characterNames = prompt (": \n 0: Spongebob | 1: Patrick | 2: Mr. Crabs | 3: Squidward") ["Spongebob", "Patrick", "Mr. Crabs", "Squidward"]; //0,1,2,3
