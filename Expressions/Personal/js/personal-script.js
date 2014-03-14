49944ef7b492291c0b547bb06049b50af9433f84
kawika valentine <kawika@kawikapedia.com>
2014-03-13 20:15:22 -0400

    personal calc #1

---
+++ Expressions/Personal/js/personal-script.js
@@ -0,0 +1,12 @@
+//Personal Calculator Script
+
+var candyAmount = prompt("This Calculator will determine how much money you spend on Ferrero Rocher chocolates each year. \nPlease enter how many Ferrero Rocher Chocolates you eat per day:");
+
+var perWeek = prompt("Please enter how many days per week you eat Ferrero Rochers:");
+
+var costPerPack  = prompt("Please enter the cost per pack of Ferrero Rochers:");
+
+var annualCost = candyAmount * perWeek * 52 * costPerPack;
+
+console.log(annualCost);
+
