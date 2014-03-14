80b33e3ba4499e71de45ab9512838af65e797310
kawika valentine <kawika@kawikapedia.com>
2014-03-13 22:06:31 -0400

    trouble shooting

--- Expressions/Expressions_Wacky/js/wacky-script.js
+++ Expressions/Expressions_Wacky/js/wacky-script.js
@@ -1,21 +1,26 @@
 //Kawika Valentine 3/14/14 Expressions Industry Specific//
 //Wacky Calculator//
 
-var colors = prompt ("This Wacky Calculator will determine just which Spongebob character you are most like. \nPlease choose your favorite color: \n 0: Yellow | 1: Red | 2: Green | 3: Pink") ["Yellow", "Red", "Green", "Pink"]; //0,1,2,3
+var colors = prompt ("This Wacky Calculator will determine just which Spongebob character you are most like. \nPlease choose your favorite color: \n 0: Yellow | 1: Red | 2: Green | 3: Pink"); //0,1,2,3
 
-var food = prompt ("Select your favorite food. \n 0: Pizza | 1: Krabby Patties | 2: Peanuts | 3: Haute Cusine") ["Pizza", "Krabby Patties", "Peanuts", "Haute Cuisine"]; //0,1,2,3
+var food = prompt ("Select your favorite food. \n 0: Pizza | 1: Krabby Patties | 2: Peanuts | 3: Haute Cusine"); //0,1,2,3
 
 var swim = prompt ("On a scale from 0-3, how well can you swim?");
 
-characterNames[4] = "Spongebob";
-characterNames[5] = "Patrick";
-characterNames[6] = "Patrick";
-characterNames[7] = "Patrick";
-characterNames[9] = "Patrick";
-characterNames[2] = "Mr. Crabs";
-characterNames[3] = "Mr. Crabs";
-characterNames[1] = "Squidward";
-characterNames[8] = "Squidward";
-                      
-var characterNames = colors + food + swim;
+
+name[4] = "Spongebob";
+name[5] = "Patrick";
+name[6] = "Patrick";
+name[9] = "Patrick";
+name[2] = "Mr. Crabs";
+name[3] = "Mr. Crabs";
+name[8] = "Squidward";
+
+var result = colors + food + swim;
+
+//This alert displays the result of the calculation//
+alert("Your Website Design quote amount is: $" + result[4] );
+
+//This will print the result to the console//
+console.log("Your Website Design quote amount is: $" + result );
 
