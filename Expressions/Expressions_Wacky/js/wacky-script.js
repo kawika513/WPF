44e227a7ebc3e550d24156d70e9504db9d58a568
kawika valentine <kawika@kawikapedia.com>
2014-03-13 21:45:03 -0400

    "" additons

--- Expressions/Expressions_Wacky/js/wacky-script.js
+++ Expressions/Expressions_Wacky/js/wacky-script.js
@@ -5,7 +5,7 @@ var colors = prompt ("This Wacky Calculator will determine just which Spongebob 
 
 var food = prompt ("Select your favorite food. \n 0: Pizza | 1: Krabby Patties | 2: Peanuts | 3: Haute Cusine") ["Pizza", "Krabby Patties", "Peanuts", "Haute Cuisine"]; //0,1,2,3
 
-var swim = prompt ("On a scale from 0-3, how well can you swim?);
+var swim = prompt ("On a scale from 0-3, how well can you swim?");
 
 characterNames[4] = "Spongebob";
 characterNames[5,6,7,9] = "Patrick";
