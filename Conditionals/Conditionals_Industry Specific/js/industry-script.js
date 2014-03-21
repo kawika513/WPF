267f043ae5c5963f27ccbc8783c2989a7a8f2b9a
kawika valentine <kawika@kawikapedia.com>
2014-03-20 22:14:44 -0400

    calculator type change

--- Conditionals/Conditionals_Industry Specific/js/industry-script.js
+++ Conditionals/Conditionals_Industry Specific/js/industry-script.js
@@ -1,21 +1,21 @@
 //Kawika Valentine 3/20/14 Expressions Industry Specific//
 //Industry Specific Conditionals Calculator//
 
-//This prompt will collect the primary color choice//
-var firstColor = prompt("This Calculator will determine which color your font should be based on your primary and secondary color choice. \nPlease choose Red, Orange, Yellow, Blue, or Green for your primary color:");
+//This prompt will collect the hours worked by the employee//
+var hoursWorked = prompt("This Calculator will determine how many more projects you can do based on the number of hours left in your week. \nPlease enter the numer of hours you've already worked this week in whole numbers:");
 
-//This prompt will collect the secondary color choice//
-var secColor = prompt("Please choose Red, Orange, Yellow, Blue, or Green for your secondary color color:");
+//This prompt will collect the number of hours the emplyee is allowed to work//
+var hoursAllowed = prompt("Please enter the number of hours you are permitted to work each week:");
 
 
 //This prompt will remind the user to enter information if they have left a prompt blank//
-if(firstColor == ""){
-    var firstColor = prompt("Ahem! You've forgotten something! \nPlease choose Red, Orange, Yellow, Blue, or Green for your primary color:");
+if(hoursWorked == ""){
+    var firstColor = prompt("Ahem! You've forgotten something! \nPlease enter the number of hours you've worked this week:");
 }
 
 //This prompt will remind the user to enter information if they have left a prompt blank//
-if(secColor == ""){
-    var more = prompt("Ahem! You've forgotten something! \nPlease choose Red, Orange, Yellow, Blue, or Green for your secondary color color:");
+if(hoursAllowed == ""){
+    var more = prompt("Ahem! You've forgotten something! \nPlease center the number of hours you are allowed to work each week:");
 }
 
 //If the drink amount is 3 or greater and the more amount is 1 or greater, display "How about some water?"//
