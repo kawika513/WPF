430d78f85131bfbbe8e13e5bef25cbee8d2b2337
kawika valentine <kawika@kawikapedia.com>
2014-03-20 21:29:10 -0400

    && values

--- Conditionals/Conditionals_Personal/js/personal-script.js
+++ Conditionals/Conditionals_Personal/js/personal-script.js
@@ -9,22 +9,27 @@ var more = prompt("Please enter the number of drinks you'd like to contnue to dr
 
 //This prompt will remind the user to enter information if they have left a prompt blank//
 if(drinkAmount = ""){
-    var drinkAmount = prompt("Ahem! You've forgotten something! \nPlease enter the number of drinks you've already had:");
+    alert("Ahem! You've forgotten something! \nPlease enter the number of drinks you've already had:");
 }
 
 if(more = ""){
-    var more = prompt("Ahem! You've forgotten something! \nPlease enter how many more drinks you'd like to have:");
+    alert("Ahem! You've forgotten something! \nPlease enter how many more drinks you'd like to have:");
 }
 
-//If the drink amount is 3 or greater, display "How about some water?"//
-if(drinkAmount >= 3){
+//If the drink amount is 3 or greater and the more amount is 1 or greater, display "How about some water?"//
+if(drinkAmount >= 3 && more >= 1){
     console.log("How about some water there slugger?");
     //This alert displays the result of the calculation//
     alert("How about some water there slugger?");
-}else{
-//If the drink amount is 2 or less, display "Perhaps just one more drink"//
+}else if(drinkAmount <= 2 && more <= 2){
+//If the drink amount is 2 or less and the more amount is 2 or less, display "Perhaps just one more drink"//
     console.log("Perhaps you've got room for just one more drink.");
     alert("Perhaps you've got room for just one more drink.");
+}else{
+//If the drink amount does not meet the above requirements, display "Bottoms up!"//
+    console.log("Bottoms up!");
+    alert("Bottoms up!");
 }
+                
 
 
