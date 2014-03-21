d547403d2807e9d2658cf78bcaaeaee49fadd378
kawika valentine <kawika@kawikapedia.com>
2014-03-20 22:09:04 -0400

    console

--- Conditionals/Conditionals_Industry Specific/js/industry-script.js
+++ Conditionals/Conditionals_Industry Specific/js/industry-script.js
@@ -19,16 +19,12 @@ if(secColor == ""){
 }
 
 //If the drink amount is 3 or greater and the more amount is 1 or greater, display "How about some water?"//
-if(drinkAmount >= 3 && more >= 1){
-    console.log("How about some water there slugger?");
+if(firstColor = Red, Blue, Green && secColor = Yellow, Green, Orange){
+    console.log("Your font color should be White.");
     //This alert displays the result of the calculation//
-    alert("How about some water there slugger?");
-}else if(drinkAmount = 2 && more <= 2){
-//If the drink amount is 2 and the more amount is 2 or less, display "Perhaps just one more drink"//
-    console.log("Perhaps you've got room for just one more drink.");
-    alert("Perhaps you've got room for just one more drink.");
-}else{
-//If the drink amount does not meet the above requirements, display "Bottoms up!"//
-    console.log("Bottoms up!");
-    alert("Bottoms up!");
+    alert("Your font color should be White.");
+}else if(firstColor = Yellow, Orange, Blue && secColor = Blue, Green, Red){
+    console.log("Your font should be Black.");
+    //This alert displays the result of the calculation//
+    alert("Your font color should be Black.");
 }
