0c025276442f0b06fd8118dac557e86c1389d302
kawika valentine <kawika@kawikapedia.com>
2014-03-20 22:49:30 -0400

    prime and sec pro pmts

--- Conditionals/Conditionals_Wacky/js/wacky-script.js
+++ Conditionals/Conditionals_Wacky/js/wacky-script.js
@@ -1,5 +1,9 @@
 //Kawika Valentine 3/20/14 Expressions Industry Specific//
 //Wacky Conditionals Calculator//
 
+//This prompt will ask for the user's shoe size//
+var shoeSize = prompt("This wacky calculator will determine the age you will die.\nPlease enter your shoe size:");
 
+//This prompt will ask for the user's mom's age//
+var momAge = prompt("Please enter your mom's age:");
 
