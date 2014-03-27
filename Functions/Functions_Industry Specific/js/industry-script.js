be9b4a23b4d9f69e9a6909159f3f063c7c469d32
kawika valentine <kawika@kawikapedia.com>
2014-03-27 12:47:36 -0400

    Added functions folder and files

---
+++ Functions/Functions_Industry Specific/js/industry-script.js
@@ -0,0 +1,33 @@
+//Kawika Valentine 3/20/14 Expressions Industry Specific//
+//Industry Specific Conditionals Calculator//
+
+//This prompt will collect the hours worked by the employee//
+var hoursWorked = prompt("This Calculator will determine how many more projects you can do based on the number of hours left in your week. \nPlease enter the numer of hours you've already worked this week in whole numbers:");
+
+//This prompt will collect the number of hours the emplyee is allowed to work//
+var hoursAllowed = prompt("Please enter the number of hours you are permitted to work each week:");
+
+
+//This prompt will remind the user to enter information if they have left a prompt blank//
+if(hoursWorked === ""){
+    var hoursAllowed = prompt("Ahem! You've forgotten something! \nPlease enter the number of hours you've worked this week:");
+}
+
+//This prompt will remind the user to enter information if they have left a prompt blank//
+if(hoursAllowed === ""){
+    var more = prompt("Ahem! You've forgotten something! \nPlease center the number of hours you are allowed to work each week:");
+}
+
+//This formula will calculate the hours remaining//
+var hoursRemain = hoursAllowed - hoursWorked;
+
+//If hours remaining is less than or equal to 15hrs, one project will be allowed//
+
+if(hoursRemain <= 15){
+    console.log("You have enough time to complete 1 project.");
+    alert("You have enough time to complete 1 project.");
+}
+if(hoursRemain >= 16){
+    console.log("You have enough time to complete at least 2 projects.");
+    alert("You have enough time to complete at least 2 projects.");
+}
---
+++ Functions/Functions_Personal/js/personal-script.js
@@ -0,0 +1,34 @@
+//Kawika Valentine 3/20/14 Conditionals Personal
+//Personal Conditional Calculator Script
+
+//This prompt will collect how many drinks the user has had//
+var drinkAmount = prompt("This Calculator will determine whether or not you're permitted to have another alcoholic drink. \nPlease enter how many alcoholic drinks you've had so far:");
+
+//This prompt will collect the number or drinks the user wants to continue to have//
+var more = prompt("Please enter the number of drinks you'd like to contnue to drink:");
+
+
+//This prompt will remind the user to enter information if they have left a prompt blank//
+if(drinkAmount == ""){
+    var drinkAmount = prompt("Ahem! You've forgotten something! \nPlease enter the number of drinks you've already had:");
+}
+
+//This prompt will remind the user to enter information if they have left a prompt blank//
+if(more == ""){
+    var more = prompt("Ahem! You've forgotten something! \nPlease enter how many more drinks you'd like to have:");
+}
+
+//If the drink amount is 3 or greater and the more amount is 1 or greater, display "How about some water?"//
+if(drinkAmount >= 3 && more >= 1){
+    console.log("How about some water there slugger?");
+    //This alert displays the result of the calculation//
+    alert("How about some water there slugger?");
+}else if(drinkAmount = 2 && more <= 2){
+//If the drink amount is 2 and the more amount is 2 or less, display "Perhaps just one more drink"//
+    console.log("Perhaps you've got room for just one more drink.");
+    alert("Perhaps you've got room for just one more drink.");
+}else{
+//If the drink amount does not meet the above requirements, display "Bottoms up!"//
+    console.log("Bottoms up!");
+    alert("Bottoms up!");
+}
